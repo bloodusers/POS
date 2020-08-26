dc45b6505c5ae6846fc51112406609ff57a0cfe2
hp <bptradecenter@gmail.com>
2020-08-26 23:05:42 +0500

    relations +Privileges+roles done

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,40 +2,41 @@
 
 @section('content')
 
-<div class="container-md step-box-left diffuse-shadow red-line-bottom white-bg wow fadeInLeft">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class=" row card-header offset-8">
-                    <div class="content-area">
+    <div class="container-md step-box-left diffuse-shadow red-line-bottom white-bg wow fadeInLeft">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class=" row card-header offset-8">
+                        <div class="content-area">
 
-                        <h2 class="green-text"> {{ __('Admin Page') }}</h2>
+                            <h2 class="green-text"> {{ __('Admin Page') }}</h2>
+                        </div>
+                    </div>
+
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+
                     </div>
                 </div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
-                    </div>
-                    @endif
-
-                </div>
             </div>
-
         </div>
     </div>
-</div>
-<div class=" container-md step-box-right diffuse-shadow red-line-bottom white-bg wow fadeInLeft">
-    <div class="row ">
+    <div class=" container-md step-box-right diffuse-shadow red-line-bottom white-bg wow fadeInLeft">
+
         <div class="col-md-8">
             <div class="card">
-                <div class=" row card-header offset-8">
-                    <div class="content-area">
+                <div class="row ">
+                    <div class=" row card-header offset-2">
+                        <div class="content-area">
 
-                        <table class="table table-hover">
+                            <table class="table table-hover content-area ">
 
-                            <thead>
+                                <thead >
                                 <th>
                                     <h6 class="green-text">id</h6>
                                 </th>
@@ -60,56 +61,56 @@
                                 <th>
                                     <h6 class="green-text">Status</h6>
                                 </th>
-                            </thead>
+                                </thead>
 
-                            <tbody>
+                                <tbody>
                                 <div class="card-body">
                                     @foreach(App\User::all() as $user)
 
-                                    <tr>
-                                        <td>{{$user->id}} </td>
-                                        <td>{{$user->name}} </td>
+                                        <tr>
+                                            <td>{{$user->id}} </td>
+                                            <td>{{$user->name}} </td>
 
-                                        <td>{{$user->shortName}} </td>
+                                            <td>{{$user->shortName}} </td>
 
-                                        <td>{{$user->contactPerson}} </td>
-                                        <td>{{$user->contact}} </td>
-                                        <td>{{$user->email}} </td>
-                                        <td>{{$user->regDate}} </td>
-                                        <td>
-                                            @if($user->isActive)
-                                            <h6 class="green-text">Activated</h6>
-                                            @else
-                                            <h6 class="red-text">Deactivated</h6>
-                                            @endif
-                                        </td>
-                                        <td>
-                                            <form method="post" action="changeStatus/{{$user->id}}">
-                                                @csrf
-                                                <button type="submit" @if($user->isActive==1)
+                                            <td>{{$user->contactPerson}} </td>
+                                            <td>{{$user->contact}} </td>
+                                            <td>{{$user->email}} </td>
+                                            <td>{{$user->regDate}} </td>
+                                            <td>
+                                                @if($user->isActive)
+                                                    <h6 class="green-text">Activated</h6>
+                                                @else
+                                                    <h6 class="red-text">Deactivated</h6>
+                                                @endif
+                                            </td>
+                                            <td>
+                                                <form method="post" action="changeStatus/{{$user->id}}">
+                                                    @csrf
+                                                    <button type="submit" @if($user->isActive==1)
                                                     class="btn red-button rad">
-                                                    {{ __('Deactivate') }}
-                                                    @else
-                                                    class="btn green-button">
-                                                    {{ __('  Ativate  ') }}
-                                                    @endif
-                                                </button>
-                                            </form>
+                                                        {{ __('Deactivate') }}
+                                                        @else
+                                                            class="btn green-button">
+                                                            {{ __('  Ativate  ') }}
+                                                        @endif
+                                                    </button>
+                                                </form>
 
-                                        </td>
+                                            </td>
 
 
-                                    </tr>
+                                        </tr>
                                     @endforeach
                                 </div>
-                            </tbody>
+                                </tbody>
 
-                        </table>
+                            </table>
 
+                        </div>
                     </div>
-                </div>
 
+                </div>
             </div>
         </div>
-    </div>
-    @endsection
+@endsection
